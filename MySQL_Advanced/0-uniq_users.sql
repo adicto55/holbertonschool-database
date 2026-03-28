9c44c530b496dccf5c5bf8e96c168ff378962588
Your Name <you@example.com>
2026-03-28 17:29:56 +0000

    ds

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,8 @@
--- Script that creates the table users
+-- Script that creates a table 'users'
+-- It will not fail if the table already exists
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
+    name VARCHAR(255),
+    PRIMARY KEY (id)
 );
