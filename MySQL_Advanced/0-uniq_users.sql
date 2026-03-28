08f99425d0f818cc68552f67420daa9af46234f2
Your Name <you@example.com>
2026-03-28 17:49:24 +0000

    sd

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,7 @@
--- Creates a table called users
+-- Creates a table users with id, email, and name attributes.
+-- Creates the table users if it does not already exist
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
